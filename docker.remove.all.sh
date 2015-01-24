236b003b967c8375a89df838028a0ff257ab3060
Evan <evan.summers@gmail.com>
2015-01-24 19:26:03 +0200

    Create docker.remove.all.sh

---
+++ docker.remove.all.sh
@@ -0,0 +1,19 @@
+c0_rm_all() {
+  for name in `docker ps -a | grep -v ^CONTAINER | cut -f1 -d' '`
+  do 
+    docker rm -f $name
+  done
+}
+
+c0_default() {
+  echo "commands: rm_all"
+}
+
+if [ $# -gt 0 ]
+then
+  command=$1
+  shift
+  c$#_$command $@
+else
+  c0_default
+fi
