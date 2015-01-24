315f50854e18bad5c0c5295b7afafda09e78b2e8
Evan <evan.summers@gmail.com>
2015-01-24 19:36:53 +0200

    Update docker.rerun.sh

--- docker.rerun.sh
+++ docker.rerun.sh
@@ -1,3 +1,8 @@
+
+cd ~/tmp/dockernodetest || exit 1
+
+[ -f Dockerfile -a -f start.sh -a -f index.js ] || exit 1
+
   docker build -t nodetest .
   docker run -d --name nodetest1 -p 8888:8888 nodetest 
   docker ps -a
