7c60b55e68497aeb8cfa9e5152e2a99fb612f485
Evan <evan.summers@gmail.com>
2015-01-25 12:35:11 +0200

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -2,5 +2,5 @@ mkdir -p ~/tmp
 cd ~/tmp 
 curl -s https://raw.githubusercontent.com/evanx/docker-node-test/master/dockertest.sh -O
 cat dockertest.sh
-sudo sh dockertest.sh
+sudo ./dockertest.sh
   
